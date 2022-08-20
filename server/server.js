a3067dd3b48a2bc5c8e951ae64d51c613ec88f18
mrint01 <sfarhatem7@yahoo.com>
2022-08-20 21:10:25 +0100

    4 commit

--- server/server.js
+++ server/server.js
@@ -30,7 +30,9 @@ mongoose
   app.use("/auth", require("./routes/profileRoutes"));
   app.use("/auth", require("./routes/todosRoutes"));
 // ----------- Public Images -----------//
-
+var distDir = __dirname + "/dist/";
+ app.use(express.static(distDir));
+ 
   app.listen(PORT, (error) =>{
     if(!error)
         console.log("Server is Successfully Running,and App is listening on port "+ PORT)
